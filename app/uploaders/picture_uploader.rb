9149411977cc862c0a822459e2e2f755c5fc2a91
Xiaos <449050350@qq.com>
2015-05-12 15:13:33 +0800

    上传图片生产环境配置2

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -9,6 +9,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+ if Rails.env.production?
+    storage :fog
+  else
     storage :file
   # storage :fog
 
